155f7481ee7142db8fde18be9d968c179a423fb1
Sonja Linton <sonja.linton@maxxpotential.com>
2018-06-13 11:54:56 -0400

    chose random word from dictionary file and created word display use for rendering in game

--- maingame.py
+++ maingame.py
@@ -15,10 +15,22 @@ HEIGHT = 400
 WHITE = (255, 255, 255)
 BLACK = (0 ,0, 0)
 
+# GAME #
+word_file = "dictionary.txt"
+WORDS = open(word_file).read().splitlines()
+word = random.choice(WORDS).upper()[1:-1]
+word_display = ""
+for char in word:
+	word_display += "_ "
+
 window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 pygame.display.set_caption('Hangman')
 
 def draw(canvas):
+	global word, word_display
+	print(word)
+	print(word_display)
+
 	canvas.fill(WHITE)
 
 while True:
