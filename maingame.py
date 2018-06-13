aa03bff8c54bab8717f86a5954ead4ebb8b895b1
Sonja Linton <sonja.linton@maxxpotential.com>
2018-06-13 11:57:21 -0400

    added event keydowns to guess letters using the keyboard

--- maingame.py
+++ maingame.py
@@ -22,19 +22,39 @@ word = random.choice(WORDS).upper()[1:-1]
 word_display = ""
 for char in word:
 	word_display += "_ "
+guess = ""
 
 window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 pygame.display.set_caption('Hangman')
 
 def draw(canvas):
 	global word, word_display
-	print(word)
-	print(word_display)
 
+	# create game canvas #
 	canvas.fill(WHITE)
+
+	if guess != "":
+		print(word)
+		print(guess)
+
+def keydown(event):
+	global guess, won
+
+	if event.unicode.isalpha():
+		guess = event.unicode.upper()
+
+init()
 
 while True:
 	draw(window)
+
+	for event in pygame.event.get():
+
+		if event.type == KEYDOWN:
+			keydown(event)
+		elif event.type == QUIT:
+			pygame.quit()
+			sys.exit()
 
 	pygame.display.update()
 	fps.tick(60)
